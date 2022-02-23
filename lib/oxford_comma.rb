1d275d4a5eff12a4812c520ebee2b4b816ea8e6c
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-23 06:40:53 -0800

    commit tree dbc5f363312e23e015ec1a328e196e22098351e2

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
 
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ")  
 end
+# binding.pry
+# oxford_comma(["kiwi", "two"])
